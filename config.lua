a8bf9703e3dd1d1b09af86cc4e0fb0ef9f505fa7
Bastien VERSINI <bastien.versini@mailbox.org>
2023-06-05 18:05:44 +0200

    config without my api key

---
+++ config.lua
@@ -0,0 +1,10 @@
+local Config = {
+  -- Your API key, find it in the settings page
+  api_key = "",
+  -- Your login cookie, used to review
+  -- Check redame for instructions on how to get it
+  sid = "",
+  -- Deck id to put the words in. Number, go to your deck's page and copy the number in the URL at the end
+  deck_id = 0,
+}
+return Config
